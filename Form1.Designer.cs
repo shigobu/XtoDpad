82371e406fd43979cbe4828f0d8dadfcc7986aa4
shigobu <shigenobu314@yahoo.co.jp>
2017-09-18 15:29:27 +0900

    メモリバグ修正最初

---
+++ Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace XtoDpad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pad1RadioButton = new System.Windows.Forms.RadioButton();
+            this.pad2RadioButton = new System.Windows.Forms.RadioButton();
+            this.pad3RadioButton = new System.Windows.Forms.RadioButton();
+            this.pad4RadioButton = new System.Windows.Forms.RadioButton();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.閉じるToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pad1RadioButton
+            // 
+            this.pad1RadioButton.AutoSize = true;
+            this.pad1RadioButton.Checked = true;
+            this.pad1RadioButton.Location = new System.Drawing.Point(13, 13);
+            this.pad1RadioButton.Name = "pad1RadioButton";
+            this.pad1RadioButton.Size = new System.Drawing.Size(47, 16);
+            this.pad1RadioButton.TabIndex = 0;
+            this.pad1RadioButton.TabStop = true;
+            this.pad1RadioButton.Text = "pad1";
+            this.pad1RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // pad2RadioButton
+            // 
+            this.pad2RadioButton.AutoSize = true;
+            this.pad2RadioButton.Location = new System.Drawing.Point(66, 13);
+            this.pad2RadioButton.Name = "pad2RadioButton";
+            this.pad2RadioButton.Size = new System.Drawing.Size(47, 16);
+            this.pad2RadioButton.TabIndex = 1;
+            this.pad2RadioButton.Text = "pad2";
+            this.pad2RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // pad3RadioButton
+            // 
+            this.pad3RadioButton.AutoSize = true;
+            this.pad3RadioButton.Location = new System.Drawing.Point(120, 13);
+            this.pad3RadioButton.Name = "pad3RadioButton";
+            this.pad3RadioButton.Size = new System.Drawing.Size(47, 16);
+            this.pad3RadioButton.TabIndex = 2;
+            this.pad3RadioButton.Text = "pad3";
+            this.pad3RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // pad4RadioButton
+            // 
+            this.pad4RadioButton.AutoSize = true;
+            this.pad4RadioButton.Location = new System.Drawing.Point(174, 13);
+            this.pad4RadioButton.Name = "pad4RadioButton";
+            this.pad4RadioButton.Size = new System.Drawing.Size(47, 16);
+            this.pad4RadioButton.TabIndex = 3;
+            this.pad4RadioButton.Text = "pad4";
+            this.pad4RadioButton.UseVisualStyleBackColor = true;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipTitle = "XtoDpad";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "XtoDpad";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.閉じるToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(105, 26);
+            // 
+            // 閉じるToolStripMenuItem
+            // 
+            this.閉じるToolStripMenuItem.Name = "閉じるToolStripMenuItem";
+            this.閉じるToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.閉じるToolStripMenuItem.Text = "閉じる";
+            this.閉じるToolStripMenuItem.Click += new System.EventHandler(this.閉じるToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(235, 42);
+            this.Controls.Add(this.pad4RadioButton);
+            this.Controls.Add(this.pad3RadioButton);
+            this.Controls.Add(this.pad2RadioButton);
+            this.Controls.Add(this.pad1RadioButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "XtoDpad";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.TextChanged += new System.EventHandler(this.Form1_TextChanged);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RadioButton pad1RadioButton;
+        private System.Windows.Forms.RadioButton pad2RadioButton;
+        private System.Windows.Forms.RadioButton pad3RadioButton;
+        private System.Windows.Forms.RadioButton pad4RadioButton;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 閉じるToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,516 @@
+﻿//#define kantan
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using vJoyInterfaceWrap;
+using SlimDX.XInput;
+
+namespace XtoDpad
+{
+    public partial class Form1 : Form
+    {
+        static public vJoy joystick;
+        static public vJoy.JoystickState iReport;
+        static public uint id = 1;
+        long maxval;
+
+        Controller[] Xcont = new Controller[4] { new Controller(UserIndex.One), new Controller(UserIndex.Two), new Controller(UserIndex.Three), new Controller(UserIndex.Four) };
+        int selectedController = 0;
+        int TriggerThreshold = 20;
+        RadioButton[] PadRadiButton = new RadioButton[4];
+
+        public Form1()
+        {
+            InitializeComponent();
+            joystick = new vJoy();
+            iReport = new vJoy.JoystickState();
+        }
+
+        bool loadingForm = false;
+        //フォームロード
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadingForm = true;
+            PadRadiButton = new RadioButton[4] { pad1RadioButton, pad2RadioButton, pad3RadioButton, pad4RadioButton };
+
+            Screen[] s = Screen.AllScreens;
+            Rectangle srect = s[1].Bounds;
+            this.Location = new Point(srect.X + srect.Width / 2 - this.Width / 2, srect.Y + srect.Height / 2 - this.Height / 2);
+
+            if (!joystick.vJoyEnabled())
+            {
+                MessageBox.Show("vJoy driver not enabled: Failed Getting vJoy attributes.");
+                this.Close();
+            }
+            if(joystick.GetVJDButtonNumber(id) < 12)
+            {
+                MessageBox.Show("仮想デバイスのボタンの数が足りません。\n" +
+                                "12ボタン以上に設定してください。", 
+                                "エラー", 
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                this.Close();
+            }
+
+            VjdStat status = joystick.GetVJDStatus(id);
+
+            if ((status == VjdStat.VJD_STAT_OWN) || ((status == VjdStat.VJD_STAT_FREE) && (!joystick.AcquireVJD(id))))
+            {
+                MessageBox.Show(string.Format("Failed to acquire vJoy device number {0}.\n", id));
+                this.Close();
+            }
+            joystick.GetVJDAxisMax(id, HID_USAGES.HID_USAGE_X, ref maxval);
+
+            joystick.RegisterRemovalCB(ChangedCB, this);
+
+            // Reset this device to default values
+            bool ret = joystick.ResetVJD(id);
+
+            this.Text = "XtoDpad Device[1]:OK";
+
+            this.WindowState = FormWindowState.Minimized;
+            loadingForm = false;
+        }
+
+        //タイマーティック
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //コントローラーの接続状態確認
+            for (int i = 0; i < 4; i++)
+            {
+                if (Xcont[i].IsConnected)
+                {
+                    PadRadiButton[i].BackColor = Color.Lime;
+                }
+                else
+                {
+                    PadRadiButton[i].BackColor = SystemColors.Control;
+                }
+            }
+
+            //選択されているコントローラーの設定
+            if (pad1RadioButton.Checked)
+            {
+                selectedController = 0;
+            }
+            else if (pad2RadioButton.Checked)
+            {
+                selectedController = 1;
+            }
+            else if (pad3RadioButton.Checked)
+            {
+                selectedController = 2;
+            }
+            else if (pad4RadioButton.Checked)
+            {
+                selectedController = 3;
+            }
+
+            if (!Xcont[selectedController].IsConnected)
+            {
+                return;
+            }
+
+            //if(!joystick.vJoyEnabled())
+            //{
+            //    return;
+            //}
+
+            if (!joystick.ResetVJD(id))
+            {
+                return;
+            }
+
+            State contState = Xcont[selectedController].GetState();
+            Gamepad Xpad = contState.Gamepad;
+            GamepadButtonFlags buttonFlags = Xpad.Buttons;
+
+#if kantan
+            //↓ボタン
+            //A
+            if (buttonFlags.HasFlag(GamepadButtonFlags.A))
+            {
+                joystick.SetBtn(true, id, 1);
+            }
+            else
+            {
+                joystick.SetBtn(false, id, 1);
+            }
+            //B
+            if (buttonFlags.HasFlag(GamepadButtonFlags.B))
+            {
+                joystick.SetBtn(true, id, 2);
+            }
+            else
+            {
+                joystick.SetBtn(false, id, 2);
+            }
+            //X
+            if (buttonFlags.HasFlag(GamepadButtonFlags.X))
+            {
+                joystick.SetBtn(true, id, 3);
+            }
+            else
+            {
+                joystick.SetBtn(false, id, 3);
+            }
+            //Y
+            if (buttonFlags.HasFlag(GamepadButtonFlags.Y))
+            {
+                joystick.SetBtn(true, id, 4);
+            }
+            else
+            {
+                joystick.SetBtn(false, id, 4);
+            }
+            //LB
+            if (buttonFlags.HasFlag(GamepadButtonFlags.LeftShoulder))
+            {
+                joystick.SetBtn(true, id, 5);
+            }
+            else
+            {
+                joystick.SetBtn(false, id, 5);
+            }
+            //RB
+            if (buttonFlags.HasFlag(GamepadButtonFlags.RightShoulder))
+            {
+                joystick.SetBtn(true, id, 6);
+            }
+            else
+            {
+                joystick.SetBtn(false, id, 6);
+            }
+            //LT
+            if (Xpad.LeftTrigger > TriggerThreshold)
+            {
+                joystick.SetBtn(true, id, 7);
+            }
+            else
+            {
+                joystick.SetBtn(false, id, 7);
+            }
+            //RT
+            if (Xpad.RightTrigger > TriggerThreshold)
+            {
+                joystick.SetBtn(true, id, 8);
+            }
+            else
+            {
+                joystick.SetBtn(false, id, 8);
+            }
+            //back
+            if (buttonFlags.HasFlag(GamepadButtonFlags.Back))
+            {
+                joystick.SetBtn(true, id, 9);
+            }
+            else
+            {
+                joystick.SetBtn(false, id, 9);
+            }
+            //start
+            if (buttonFlags.HasFlag(GamepadButtonFlags.Start))
+            {
+                joystick.SetBtn(true, id, 10);
+            }
+            else
+            {
+                joystick.SetBtn(false, id, 10);
+            }
+            //LSB
+            if (buttonFlags.HasFlag(GamepadButtonFlags.LeftThumb))
+            {
+                joystick.SetBtn(true, id, 11);
+            }
+            else
+            {
+                joystick.SetBtn(false, id, 11);
+            }
+            //RSB
+            if (buttonFlags.HasFlag(GamepadButtonFlags.RightThumb))
+            {
+                joystick.SetBtn(true, id, 12);
+            }
+            else
+            {
+                joystick.SetBtn(false, id, 12);
+            }
+            //十字キー
+            //右上
+            if (buttonFlags.HasFlag(GamepadButtonFlags.DPadRight) && buttonFlags.HasFlag(GamepadButtonFlags.DPadUp))
+            {
+                joystick.SetContPov(4500, id, 1);
+            }
+            //右下
+            else if(buttonFlags.HasFlag(GamepadButtonFlags.DPadRight) && buttonFlags.HasFlag(GamepadButtonFlags.DPadDown))
+            {
+                joystick.SetContPov(13500, id, 1);
+            }
+            //左下
+            else if(buttonFlags.HasFlag(GamepadButtonFlags.DPadLeft) && buttonFlags.HasFlag(GamepadButtonFlags.DPadDown))
+            {
+                joystick.SetContPov(22500, id, 1);
+            }
+            //左上
+            else if(buttonFlags.HasFlag(GamepadButtonFlags.DPadLeft) && buttonFlags.HasFlag(GamepadButtonFlags.DPadUp))
+            {
+                joystick.SetContPov(31500, id, 1);
+            }
+            //上
+            else if(buttonFlags.HasFlag(GamepadButtonFlags.DPadUp))
+            {
+                joystick.SetContPov(0, id, 1);
+            }
+            //右
+            else if(buttonFlags.HasFlag(GamepadButtonFlags.DPadRight))
+            {
+                joystick.SetContPov(9000, id, 1);
+            }
+            //下
+            else if (buttonFlags.HasFlag(GamepadButtonFlags.DPadDown))
+            {
+                joystick.SetContPov(18000, id, 1);
+            }
+            //左
+            else if (buttonFlags.HasFlag(GamepadButtonFlags.DPadLeft))
+            {
+                joystick.SetContPov(27000, id, 1);
+            }
+            //ニュートラル
+            else
+            {
+                joystick.SetContPov(-1, id, 1);
+            }
+
+            //アナログ
+            //左横軸
+            joystick.SetAxis(Xpad.LeftThumbX / 2 + (int)maxval / 2, id, HID_USAGES.HID_USAGE_X);
+            //左縦軸
+            joystick.SetAxis(Xpad.LeftThumbY / 2 + (int)maxval / 2, id, HID_USAGES.HID_USAGE_Y);
+            //右横軸
+            joystick.SetAxis(Xpad.RightThumbX / 2 + (int)maxval / 2, id, HID_USAGES.HID_USAGE_RX);
+            //右縦軸
+            joystick.SetAxis(Xpad.RightThumbY / 2 + (int)maxval / 2, id, HID_USAGES.HID_USAGE_RY);
+#else
+            vJoy.JoystickState iReport = new vJoy.JoystickState();
+            iReport.bDevice = (byte)id;
+            uint VJbuttonFlags = 0;
+
+            //↓ボタン
+            //A
+            if (buttonFlags.HasFlag(GamepadButtonFlags.A))
+            {
+                VJbuttonFlags = VJbuttonFlags | 1;
+            }
+            //B
+            if (buttonFlags.HasFlag(GamepadButtonFlags.B))
+            {
+                VJbuttonFlags = VJbuttonFlags | 1 << 1;
+            }
+            //X
+            if (buttonFlags.HasFlag(GamepadButtonFlags.X))
+            {
+                VJbuttonFlags = VJbuttonFlags | 1 << 2;
+            }
+            //Y
+            if (buttonFlags.HasFlag(GamepadButtonFlags.Y))
+            {
+                VJbuttonFlags = VJbuttonFlags | 1 << 3;
+            }
+            //LB
+            if (buttonFlags.HasFlag(GamepadButtonFlags.LeftShoulder))
+            {
+                VJbuttonFlags = VJbuttonFlags | 1 << 4;
+            }
+            //RB
+            if (buttonFlags.HasFlag(GamepadButtonFlags.RightShoulder))
+            {
+                VJbuttonFlags = VJbuttonFlags | 1 << 5;
+            }
+            //LT
+            if (Xpad.LeftTrigger > TriggerThreshold)
+            {
+                VJbuttonFlags = VJbuttonFlags | 1 << 6;
+            }
+            //RT
+            if (Xpad.RightTrigger > TriggerThreshold)
+            {
+                VJbuttonFlags = VJbuttonFlags | 1 << 7;
+            }
+            //back
+            if (buttonFlags.HasFlag(GamepadButtonFlags.Back))
+            {
+                VJbuttonFlags = VJbuttonFlags | 1 << 8;
+            }
+            //start
+            if (buttonFlags.HasFlag(GamepadButtonFlags.Start))
+            {
+                VJbuttonFlags = VJbuttonFlags | 1 << 9;
+            }
+            //LSB
+            if (buttonFlags.HasFlag(GamepadButtonFlags.LeftThumb))
+            {
+                VJbuttonFlags = VJbuttonFlags | 1 << 10;
+            }
+            //RSB
+            if (buttonFlags.HasFlag(GamepadButtonFlags.RightThumb))
+            {
+                VJbuttonFlags = VJbuttonFlags | 1 << 11;
+            }
+            //ボタンフラグ登録
+            iReport.Buttons = VJbuttonFlags;
+            //十字キー
+            //右上
+            if (buttonFlags.HasFlag(GamepadButtonFlags.DPadRight) && buttonFlags.HasFlag(GamepadButtonFlags.DPadUp))
+            {
+                iReport.bHats = 4500;
+            }
+            //右下
+            else if(buttonFlags.HasFlag(GamepadButtonFlags.DPadRight) && buttonFlags.HasFlag(GamepadButtonFlags.DPadDown))
+            {
+                iReport.bHats = 13500;
+            }
+            //左下
+            else if(buttonFlags.HasFlag(GamepadButtonFlags.DPadLeft) && buttonFlags.HasFlag(GamepadButtonFlags.DPadDown))
+            {
+                iReport.bHats = 22500;
+            }
+            //左上
+            else if(buttonFlags.HasFlag(GamepadButtonFlags.DPadLeft) && buttonFlags.HasFlag(GamepadButtonFlags.DPadUp))
+            {
+                iReport.bHats = 31500;
+            }
+            //上
+            else if(buttonFlags.HasFlag(GamepadButtonFlags.DPadUp))
+            {
+                joystick.SetContPov(0, id, 1);
+                iReport.bHats = 0;
+            }
+            //右
+            else if(buttonFlags.HasFlag(GamepadButtonFlags.DPadRight))
+            {
+                iReport.bHats = 9000;
+            }
+            //下
+            else if (buttonFlags.HasFlag(GamepadButtonFlags.DPadDown))
+            {
+                iReport.bHats = 18000;
+            }
+            //左
+            else if (buttonFlags.HasFlag(GamepadButtonFlags.DPadLeft))
+            {
+                iReport.bHats = 27000;
+            }
+            //ニュートラル
+            else
+            {
+                iReport.bHats = 0xFFFFFFFF;
+            }
+
+            //アナログ
+            //左横軸
+            iReport.AxisX = Xpad.LeftThumbX / 2 + (int)maxval / 2;
+            //左縦軸
+            iReport.AxisY = Xpad.LeftThumbY / 2 + (int)maxval / 2;
+            //右横軸
+            iReport.AxisXRot = Xpad.RightThumbX / 2 + (int)maxval / 2;
+            //右縦軸
+            iReport.AxisYRot = Xpad.RightThumbY / 2 + (int)maxval / 2;
+
+            //更新
+            bool ret = joystick.UpdateVJD(id, ref iReport);
+
+#endif
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            joystick.RelinquishVJD(id);
+        }
+
+        private void ChangedCB(bool Removed, bool First, object userData)
+        {
+            Form1 f = (Form1)userData;
+            string title = "XtoDpad";
+
+            if( !Removed && !First)
+            {
+                title += " Device[1]:OK";
+                f.setTitle(title);
+                bool ret = joystick.AcquireVJD(id);
+            }
+            else
+            {
+                title += " Device[1]:Wait...";
+                f.setTitle(title);
+            }
+        }
+
+        delegate void setTitleDelegate(string title);
+
+        private void setTitle(string title)
+        {
+            if(InvokeRequired)
+            {
+                Invoke(new setTitleDelegate(setTitle),title);
+                return;
+            }
+            this.Text = title;
+        }
+
+        private void Form1_TextChanged(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipText = this.Text;
+            notifyIcon1.ShowBalloonTip(10000);
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.ShowInTaskbar = false;
+                if(!loadingForm)
+                {
+                    notifyIcon1.BalloonTipText = "タスクトレイに格納されました";
+                    notifyIcon1.ShowBalloonTip(10000);
+                }
+            }
+            else
+            {
+                this.ShowInTaskbar = true;
+            }
+        }
+
+        bool FormCloseOK = false;
+        private void 閉じるToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormCloseOK = true;
+            this.Close();
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!FormCloseOK)
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+    }
+}
